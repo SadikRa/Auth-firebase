8499c318cde20bc84154823abdf1e3fc34b4f0f4
SadikRa <sadikrahman494@gmail.com>
2024-09-27 22:32:13 +0600

    register page added done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Register from '../Register/Register';
 
 const Home = () => {
     return (
         <div>
             <h6 className='text-6xl text-red-600'>my name is sadik</h6>
+            <Register></Register>
         </div>
     );
 };
